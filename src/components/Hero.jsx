9c475d5714723d92f29aa50441f46b7690eb9798
Sanvi Deep <sanvideep2112@gmail.com>
2026-01-19 16:53:59 +0530

    Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const hero = () => {
+  return (
+    <div className='relative h-dhv'>
+      <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
+        <div>   
+            <div className='mask-clip-path'>    
+            </div>
+        </div> 
+      </div>
+    </div>
+  )
+}
+
+export default hero
